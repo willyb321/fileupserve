47071e622adb078d39c6d7c77b1a9c595e5b88c5
willyb321 <wbwilliam7@gmail.com>
2017-10-02 18:26:01 +1100

    do a basic auth

--- routes/upload.js
+++ routes/upload.js
@@ -3,18 +3,25 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({dest: require('path').join(__dirname, '..', 'uploads')});
 const {insertImg} = require('./dbutils');
+const token = process.env.FILEUPSERVE_TOKEN;
 router.post('/', upload.single('imageData'), (req, res, next) => {
 	let id = req.file.filename;
-	insertImg(id, req.file.filename, req.file.path)
-		.then(data => {
-			if (data.inserted === true) {
-				const url = `https://${req.get('X-Forwarded-Host') || req.get('host')}/i/${id}`;
-				res.json({done: true, url});
-			}
-		})
-		.catch(err => {
-			console.log(err);
-		})
+	const isAuthorised = (req.body.token === token);
+	if (isAuthorised) {
+		insertImg(id, req.file.filename, req.file.path)
+			.then(data => {
+				if (data.inserted === true) {
+					const url = `https://${req.get('X-Forwarded-Host') || req.get('host')}/i/${id}`;
+					res.json({done: true, url});
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	} else {
+		res.status(403);
+		res.end();
+	}
 });
 
 module.exports = router;
