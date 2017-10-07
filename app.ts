757d57dbeb03c471ce0b3fc1f8adf2ce3bf84046
willyb321 <wbwilliam7@gmail.com>
2017-10-08 09:15:32 +1100

    terrible image site that will probably break a lot

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
 import * as logger from "morgan";
 import * as cookieParser from "cookie-parser";
+import * as sassMiddleware from 'node-sass-middleware'
+import * as favicon from 'serve-favicon'
 import * as bodyParser from "body-parser";
 import index from "./routes/index";
 import upload from "./routes/upload";
 import img from "./routes/img";
+import {join} from "path";
 
 const app = express();
 app.use(logger('dev'));
@@ -12,6 +15,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
+// view engine setup
+app.set('views', join(__dirname, '..', 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(sassMiddleware({
+	src: join(__dirname, '..', 'public'),
+	dest: join(__dirname, '..', 'public'),
+	indentedSyntax: true, // true = .sass and false = .scss
+	sourceMap: true
+}));
+app.use(express.static(join(__dirname, '..', 'public')));
+app.use('/thumbs/:id', express.static(join(__dirname, '..', 'public', 'thumbs')))
 app.use('/', index);
 app.use('/i', img);
 app.use('/upload', upload);
--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,54 @@
 ///<reference path="../node_modules/@types/node/index.d.ts"/>
 import * as express from 'express';
+import {join, parse} from "path";
+import * as klawSync from 'klaw-sync';
+import * as sharp from 'sharp';
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-	res.status(403);
-	res.end('<p>nah</p>')
+	getThumbsForGallery()
+		.then(thumbs => {
+			res.render('index', {
+				thumbs: thumbs,
+				title: 'images and stuff'
+			})
+		})
 });
-
+const thumbsPath = join(__dirname, '..', '..', 'public', 'thumbs');
+function getThumbsForGallery() {
+	return new Promise<Array<any>>(async (resolve, reject) => {
+		const filesOrig = klawSync(join(__dirname, '..', 'uploads'), {nodir: true});
+		const thumbsOrig = klawSync(join(__dirname, '..', '..', 'public', 'thumbs'), {nodir: true});
+		let thumbs = [];
+		// console.log(filesOrig);
+		thumbsOrig.forEach(thumb => {
+			filesOrig.forEach((file, ind) => {
+				filesOrig[ind].thumbed = parse(file.path).base === parse(thumb.path).base;
+				file.index = ind;
+			})
+		});
+		for (const file of filesOrig) {
+			if (!file.thumbed) {
+				await sharp(file.path)
+					.resize(320, 240)
+					.toFile(join(thumbsPath, parse(file.path).base))
+					.then(info => {
+						info.path = `/thumbs/${parse(file.path).base}`;
+						info.properURL = `/i/${parse(file.path).base}`;
+						thumbs.push(info);
+					})
+					.catch(err => {
+						console.log(err);
+					})
+			} else {
+				file.path = `/thumbs/${parse(file.path).base}`;
+				file.properURL = `/i/${parse(file.path).base}`;
+				thumbs.push(file);
+			}
+		}
+		resolve(thumbs);
+	})
+}
 export default router;
